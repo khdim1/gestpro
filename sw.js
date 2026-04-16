5e705c059cb4a3e5a79069ef7003a9fe1345af23
khdim1 <nkhadim066@gmail.com>
2026-04-16 02:11:41 +0000

    Correction PWA

--- sw.js
+++ sw.js
@@ -4,8 +4,7 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css',
-  'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js'
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css'
 ];
 
 self.addEventListener('install', event => {
